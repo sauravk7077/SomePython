5bd7d99acbabb0dab008140cb3a535117ecf9e94
sauravk7077 <sauravk7077@gmail.com>
2020-05-12 09:37:30 +0530

    added documentation

--- message_display.py
+++ message_display.py
@@ -3,7 +3,12 @@ from colorama import init,Fore
 init()
 
 
-'''This method displays the message in different colors depending on the info_level'''
+"""
+This method displays the message in different colors depending on the info_level
+Parameters:
+    message (str): The message to be displayed
+    info_level: The values are 'info', 'warning', 'error'. These values determine the color of message.
+"""
 def show_info(message, info_level='info'):
     if info_level.lower() == "error":
         print(Fore.RED + "Error!")
